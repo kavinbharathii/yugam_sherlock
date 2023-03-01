2489b3eb60ac60224f4befc2e7bc9855e05cc3f6
Kavin Bharathi <r.m.kavinbharathi@gmail.com>
2023-03-01 10:13:01 +0530

    initial

---
+++ README.md
@@ -0,0 +1,8 @@
+
+###Sher
+
+<!-- sher README -->
+Author: 
+Version: 1.0.0
+Description: sher
+    
---
+++ app.js
@@ -0,0 +1,4 @@
+
+console.log('Sher successfully!')
+const dev = document.getElementById('dev')
+    
