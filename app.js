530a9a3663f7fb6144829f2dcd69e5bc6a37d9e5
Kavin Bharathi <66783110+kavinbharathii@users.noreply.github.com>
2023-03-02 18:18:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 
-console.log('Sher successfully!')
 const dev = document.getElementById('dev')
-    
+const kore = document.getElementById('kore')
+const button = document.getElementById('confirm')
+const answer = document.getElementById('answer')
+
+button.addEventListener('click', () => {
+    if (kore.value == "prime") {
+        console.log("Yes!")
+        answer.innerHTML = "Next location: Kore"
+    } else {
+        console.log("Atleast logging works!")
+        answer.innerHTML = "Nope"
+    }
+})
